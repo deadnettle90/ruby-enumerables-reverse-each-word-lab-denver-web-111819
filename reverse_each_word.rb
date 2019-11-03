3815958e3de9758965252ad782b806b605865866
Danielle Matton <danimatton@gmail.com>
2019-11-03 22:04:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ string = ["I'm, trying, to, learn, Ruby!"]
 reverse_each_word << string.split(/,/)
 
 def reverse_each_word.each do |reverse_each_word|
-  reverse_each_word.
+  reverse_each_word.reverse_each
 end
